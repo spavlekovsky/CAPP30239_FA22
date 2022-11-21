93ef8bd3779c74ff47fbcff8b495de61ea90fb06
Sam Pavlekovsky <spavlekovsky@Sams-MacBook-Pro.local>
2022-11-21 01:20:01 -0600

    more charts work

---
+++ final/scatter.js
@@ -0,0 +1,74 @@
+
+
+d3.csv('../data/wb_join.csv').then((data) => {
+    const height = 400,
+        width = 600,
+        margin = ({ top: 25, right: 30, bottom: 50, left: 30 }),
+        padding = 1;
+
+    const svg2 = d3.select("#refugees_wb")
+        .append("svg")
+        .attr("viewBox", [0, 0, width, height]);
+
+    let timeParse = d3.timeParse("%Y");
+    let timeFormat = d3.timeFormat("%Y")
+
+    for (let r of data) {
+        r.Year = timeParse(r.Year);
+        r.Origin = +r.Origin
+        r.Asylum = +r.Asylum
+    }
+
+    console.log(data)
+
+    let x = d3.scaleLog()
+        .domain(d3.extent(data, d => d.Origin)).nice()
+        .range([margin.left, width - margin.right]);
+
+    let y = d3.scaleLog()
+        .domain(d3.extent(data, d => d.Asylum)).nice()
+        .range([height - margin.bottom, margin.top]);
+
+    svg2.append("g")
+        .attr("transform", `translate(0,${height - margin.bottom})`)
+        .attr("class", "x-axis")
+        .call(d3.axisBottom(x).tickSize(0))
+
+    svg2.append("g")
+        .attr("transform", `translate(${margin.left},0)`)
+        .attr("class", "y-axis")
+        .call(d3.axisLeft(y).tickSize(0))
+
+    svg2.append("g")
+        .attr("fill", "black")
+        .selectAll("circle")
+        .data(data)
+        .join("circle") // joining dta to each circle object
+        .attr("cx", d => x(d.Origin)) // cx & cy position from *center* of x, y
+        .attr("cy", d => y(d.Asylum))
+        .attr("r", 2) // radius
+        .attr("opacity", 0.75);
+
+    const tooltip = d3.select("body").append("div")
+        .attr("class", "svg-tooltip")
+        .style("position", "absolute")
+        .style("visibility", "hidden");
+
+    d3.selectAll("circle") // create an "event listener"
+        .on("mouseover", function(event, d) { // wait until something happens then pass in the thing and the data
+          d3.select(this).attr("fill", "red"); // select the thing you're on
+          tooltip                              // do something with the previously defined tooltip
+            .style("visibility", "visible")
+            .html(`${d.Country} ${timeFormat(d.Year)}`);
+        })
+        .on("mousemove", function(event) { // this part makes the text follow the mouse/hover over the point
+          tooltip
+            .style("top", (event.pageY - 20) + "px")
+            .style("left", (event.pageX + 10) + "px");
+        })
+        .on("mouseout", function() { // this part makes the point turn back to black so you don't keep all the tooltips
+          d3.select(this).attr("fill", "black");
+          tooltip.style("visibility", "hidden");
+        })
+
+});
--- final/water.js
+++ final/water.js
@@ -1,9 +1,9 @@
 const height = 400,
     width = 600,
-    margin = ({ top: 25, right: 10, bottom: 50, left: 10 }),
+    margin = ({ top: 25, right: 15, bottom: 50, left: 15 }),
     padding = 1;
 
-const svg = d3.select("#conflicts_by_country")
+const svg = d3.select("#conflicts_refugees")
     .append("svg")
     .attr("viewBox", [0, 0, width, height]);
 
@@ -12,10 +12,145 @@ Promise.all([
   d3.csv('../data/water_agg.csv'),
   d3.csv('../data/un_mena_water_agg.csv'),
 ]).then((data) => {
-  console.log(data)
+  let timeParse = d3.timeParse("%Y");
 
-})
+  const years = ['2010', '2011', '2012','2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022']
 
+  for (let r of data[0]) {
+    // if (r.Year === 'Total') {
+    //   r.Year = null
+    //   r.Total = true
+    // } else {
+    //   r.Year = timeParse(r.Year);
+    //   r.Total = false
+    // }
+    if (r.Year != 'Total') {
+      r.Year = +r.Year
+    }
+    r.Count = +r.Count;
+  }
+  for (let r of data[1]) {
+    for (let y of years) {
+      r[y] = +r[y]
+    }
+  }
+
+  // console.log(data[0])
+  // // console.log('hi')
+  // console.log(data[1])
+
+
+  function make_chart(country) {
+      data_w = data[0].filter(d => d.Country === country && d.Year != 'Total')
+      data_r = data[1].filter(d => d['Country of origin'] === country)
+
+      console.log(data_w)
+
+      const x = d3.scaleBand()
+        .domain(data_w.map(d => d.Year))
+        .range([margin.left, width - margin.right])
+        .padding(.5);
+
+      console.log(data_w.map(d => d.Year))
+
+      // const x = d3.scaleTime()
+      //     .range([margin.left, width - margin.right])
+      //     .domain(d3.extent(data_w, d => d.Year));
+
+      const y = d3.scaleLinear()
+          .range([height/2, height - margin.bottom])
+          .domain(d3.extent(data_w, d => d.Count)).nice();
+
+      svg.append("g")
+          .attr("transform", `translate(0,${height/2})`)
+          .call(d3.axisBottom(x));
+
+      svg.append("g")
+          .attr("transform", `translate(${margin.left},0)`)
+          .call(d3.axisLeft(y).tickSize(-innerWidth));
+
+      // let line = d3.line()
+      //   .x(d => x(d.Year))
+      //   .y(d => y(d.Count));
+      //   // line defaults to a curvy shape but you can change it to be more step-wise or such
+
+      // svg.append("path")
+      //     .datum(data_w) // only one path/line so we use datUM
+      //     .attr("d", line)
+      //     .attr("fill", "none")
+      //     .attr("stroke", 'steelblue');
+
+      // const binGroups = svg.append("g")
+      //     .attr("class", "bin-group");
+
+      // const bins = d3.bin()
+      //     .thresholds(10)
+      //     .value(d => d.average)(data[i]);
+
+      // binGroups.selectAll("g")
+      //     .data(bins, d => d.x0)
+      // svg.append("path")
+
+      let bar = svg.append("g")
+        .attr("class", "bar")
+
+      bar.selectAll("g")
+        .data(data_w)
+        .join(
+          enter => {
+          let g = enter.append("g")
+
+          console.log('enter1')
+
+          g.append("rect")
+            .attr("fill", "steelblue")
+            .attr("x", d => x(d.Year))
+            .attr("width", x.bandwidth())
+            .attr("y", y(0))
+            .transition()
+            .duration(750)
+            .attr("y", d => y(0))
+            .attr("height", d => y(d.Count) - y(0));
+
+            console.log('enter2');
+          },
+          update => {
+            console.log('update')
+          update.select("rect")
+              .transition()
+              .duration(750)
+              .attr("y", d => y(d.length))
+              .attr("height", d => height - margin.bottom - y(d.length));
+          },
+          exit => {
+            console.log('exit')
+          exit.select("rect")
+              .transition()
+              .duration(750)
+              .attr("height", 0)
+              .attr("y", height/2);
+
+          exit.select("text")
+              .text("");
+
+          exit.transition()
+              .duration(750)
+              .remove();
+          }
+        );
+  }
+
+  make_chart('Algeria');
+
+  d3.selectAll("select")
+      .on("change", function (event) {
+
+      console.log(event.target.value)
+      // const country = event.target.value;
+      make_chart(event.target.value);
+  });
+
+});
 
 // d3.json('../data/World-Water-Conflicts-2022.csv').then((data) => {
 
